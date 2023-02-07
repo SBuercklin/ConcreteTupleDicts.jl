d44d3c6b5b1ac33bf6d38d1fc4c740e7408b9f29
Sam Buercklin <sam.buercklin@gmail.com>
2023-02-06 20:22:20 -0500

    implement ambiguity checks

--- src/TupleDicts.jl
+++ src/TupleDicts.jl
@@ -4,24 +4,65 @@ using Base
 
 export TupleDict
 
+"""
+    struct TupleDict{TD, K, V} <: AbstractDict{K, V}
+
+A `Dict`-like structure which maps keys of various types to values of various types
+    in a type stable manner. Crucially, for every key of type `TK`, it must map to
+    a value of type `TV`. 
+
+With a traditional `Dict`, keys of distinct types mapping to values of distinct types
+    cannot lead to type stable retrieval. However, a `TupleDict` is type stable, 
+    even when the `valtype` for each `keytype` is different. 
+"""
 struct TupleDict{TD <: Tuple, K, V} <: AbstractDict{K, V}
     dicts::TD
     function TupleDict{TD}(td::TD) where {TD}
         ks = (keytype(d) for d in td)
-        vs = (eltype(values(d)) for d in td)
+        vs = (valtype(d) for d in td)
         K = Union{ks...}
         V = Union{vs...}
 
         return new{TD, K, V}(td)
     end
 end
-function TupleDict(td::TD) where {TD}
-    return TupleDict{TD}(td)
-end
 
-function _find_dict(_d::TupleDict, ::T) where {T}
-    d_idx = findfirst(d -> T <: keytype(d), _d.dicts)
-    return _d.dicts[d_idx]
+function TupleDict(td)
+    # Only want to work with concrete types to avoid ambgiuities in the Dict retrieval
+    if !all(_concrete_or_union, td)
+        throw(
+            error(
+                "All input `Dict` keytypes to a `TupleDict` must be concrete types, or union of concrete types"
+                )
+            )
+    end
+    # Duplicated key types with different value types also means ambiguity
+    if _duplicated_key_types(td)
+        throw(
+            error(
+                "All input `Dict` keytypes to a `TupleDict` must be unique. Duplicate keys will cause ambiguity"
+                )
+            )
+    end
+
+    valtypes = unique(valtype.(td))
+
+    reduced_dicts = map(valtypes) do v
+        idxs = findall(isequal(v), valtype.(td))
+        keytypes = keytype.(td[idxs])
+        merged_dicts = mapreduce(d -> convert(Dict{Union{keytypes...}, v}, d), merge, td[idxs])
+
+        return merged_dicts
+    end
+
+    tuple_dicts = Tuple(reduced_dicts)
+
+    if length(tuple_dicts) == 1
+        return only(tuple_dicts) # That's just a regular Dict wrapped in a Tuple
+    else
+        TupleDict{typeof(tuple_dicts)}(tuple_dicts)
+    end
+
 end
 
 function Base.getindex(_d::TupleDict, k)
@@ -34,13 +75,9 @@ function Base.setindex!(_d::TupleDict, v, k)
     return Base.setindex!(d, v, k)
 end
 
-function Base.keys(td::TupleDict)
-    mapreduce(keys, union, td.dicts)
-end
+Base.keys(td::TupleDict) = mapreduce(keys, union, td.dicts)
 
-function Base.values(td::TupleDict)
-    mapreduce(values, union, td.dicts)
-end
+Base.values(td::TupleDict) = mapreduce(values, union, td.dicts)
 
 function Base.iterate(td::TupleDict, st = (0, 1, 0))
     # i is the index of the Dict in the tuple 
@@ -62,5 +99,58 @@ function Base.iterate(td::TupleDict, st = (0, 1, 0))
 end
 
 Base.length(td::TupleDict) = length(keys(td))
+
+
+#=
+    Utilities, helpers
+=#
+
+function _concrete_or_union(::AbstractDict{TK, TV}) where {TK, TV}
+    if isconcretetype(TK) 
+        return true
+    elseif TK isa Union
+        return all(isconcretetype, fieldnames(TK))
+    else
+        return false
+    end
+end
+
+function _duplicated_key_types(ds)
+    state = iterate(ds)
+    while !isnothing(state)
+        next_it, _state = state
+        remainder = Base.rest(ds, _state)
+        if any(_conflicting_kvs(next_it), remainder)
+            return true
+        end
+        state = iterate(ds, _state)
+    end
+    return false
+end
+
+_conflicting_kvs(a) = Base.Fix1(_conflicting_kvs,a)
+
+# 1. No problem if the value types are the same, we'll merge Dicts later
+# 2. No problem if the keys don't intersect
+# 3. if the valtypes differ but the keytypes intersect, could be ambiguous
+function _conflicting_kvs(a, b)
+    if valtype(a) == valtype(b)
+        return false
+    elseif typeintersect(keytype(a), keytype(b)) == Union{}
+        return false
+    else
+        return true
+    end
+end
+_conflicting_kvs(a, b::Nothing) = false
+
+
+# Crucially, this is O(1) thanks to the Julia compiler evaluating `findfirst` in constant time
+#   over the tuple _d.dicts
+function _find_dict(_d::TupleDict, ::T) where {T}
+    d_idx = findfirst(d -> T <: keytype(d), _d.dicts)
+
+    return _d.dicts[d_idx]
+end
 
 end # module TupleDict
